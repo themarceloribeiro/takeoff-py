9d495a4783479d6d1d7d29e519e009e7b9249682
Marcelo Ribeiro <me@marceloribeiro.us>
2020-10-13 02:50:03 -0700

    Functional class in package

---
+++ build/lib/takeoff/generator.py
@@ -0,0 +1,3 @@
+class Generator:
+    def print_instructions(self):
+        print('This package helps with your project takeoff by creating views, models and project structure based off of JSON files')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="takeoff-py",
-    version="0.0.2",
+    version="0.0.3",
     author="Marcelo Ribeiro",
     author_email="themarceloribeiro@gmail.com",
     description="A Python toolset to launch full stack, mobile projects",
