cc9db1c4009590eecf1463236278c8959b4a0aee
Marcelo Ribeiro <me@marceloribeiro.us>
2020-10-13 03:10:04 -0700

    Added the takeoff-generate script

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="takeoff-py",
-    version="0.0.3",
+    scripts=['takeoff-generate'],
+    version="0.0.4",
     author="Marcelo Ribeiro",
     author_email="themarceloribeiro@gmail.com",
     description="A Python toolset to launch full stack, mobile projects",
