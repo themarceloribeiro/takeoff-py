21bb90dbee836f2e8ab311c777fcc71e3d8c937f
Marcelo Ribeiro <me@marceloribeiro.us>
2020-10-16 10:12:04 -0700

    Make migrations from models; Models inside models folder

--- takeoff/generators/web/web_model_generator.py
+++ takeoff/generators/web/web_model_generator.py
@@ -1,6 +1,7 @@
 import os
 from jinja2 import Template
 from ..generator_base import GeneratorBase
+from pathlib import Path
 
 class WebModelGenerator(GeneratorBase):
     def __init__(self, name, options):
@@ -8,6 +9,9 @@ class WebModelGenerator(GeneratorBase):
         self.options = options
         self.model_name = ''
         self.model_attributes = []
+
+    def project_folder(self):
+        return f"dist/{self.name}/web/{self.name}"
 
     def run(self):
         self.model_name = self.options.pop(0)
@@ -22,6 +26,8 @@ class WebModelGenerator(GeneratorBase):
             })
 
         self.write_model_file()
+        self.update_models_file()        
+        self.generate_migration()
     
     def attribute_class(self, type):
         switcher = { 
@@ -39,8 +45,9 @@ class WebModelGenerator(GeneratorBase):
 
     def write_model_file(self):
         template_path = '/Users/marcelo/work/takeoff/python/takeoff/takeoff/templates/web/model.template'
-        destination_folder = f"dist/{self.name}/web/{self.name}/main/models"
+        destination_folder = f"{self.project_folder()}/main/models"
         os.system(f"mkdir -p {destination_folder}")
+        Path(f"{destination_folder}/__init__.py").touch()
         destination = f"{destination_folder}/{self.model_name}.py"
 
         with open(template_path) as f:
@@ -48,5 +55,21 @@ class WebModelGenerator(GeneratorBase):
 
         template = Template(template_contents)
         contents = template.render(generator=self)
+        
         with open(destination, 'w') as f:
             f.write(contents)
+
+    def update_models_file(self):
+        current_file = f"{self.project_folder()}/main/models/__init__.py"
+
+        lines = list(open(current_file, 'r'))
+        lines.append(f"from .{self.model_name} import {self.model_name.replace('_', ' ').title().replace(' ', '')}\n")
+        
+        with open(current_file, 'w') as file:
+            file.writelines(lines)
+
+    def models_last_line(self, lines):
+        return len(lines)
+    
+    def generate_migration(self):
+        os.system(f"cd {self.project_folder()} && python3 manage.py makemigrations")
