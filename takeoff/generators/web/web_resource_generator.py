4c5c8b89af810dcccfe67b1c712f06d8b12ae868
Marcelo Ribeiro <me@marceloribeiro.us>
2020-10-25 05:50:49 -0700

    Added root path/index file

--- takeoff/generators/web/web_resource_generator.py
+++ takeoff/generators/web/web_resource_generator.py
@@ -21,6 +21,7 @@ class WebResourceGenerator(GeneratorBase):
 
         self.generate_layout()
         self.generate_nav()
+        self.generate_root()
         self.load_model_attributes()
         self.write_view_file()
         self.update_views_file()
@@ -28,8 +29,8 @@ class WebResourceGenerator(GeneratorBase):
         self.write_views()
         self.prepare_urls()
 
-    def render_template(self, template_path, destination):
-        if os.path.exists(destination):
+    def render_template(self, template_path, destination, overwrite=False):
+        if os.path.exists(destination) and not overwrite:
             return
         
         with open(template_path) as f:
@@ -47,12 +48,42 @@ class WebResourceGenerator(GeneratorBase):
         template_path = f"{self.templates_path}/web/views/layout.html.template"
         self.render_template(template_path, destination)
 
+    def generate_root(self):
+        main = f"{self.project_folder()}/main/templates/main"
+        os.system(f"mkdir -p {main}")
+        destination = f"{main}/index.html"
+        template_path = f"{self.templates_path}/web/views/root.html.template"
+        self.render_template(template_path, destination)
+        self.render_main_view_template()
+
     def generate_nav(self):
         shared = f"{self.project_folder()}/main/templates/shared"
         os.system(f"mkdir -p {shared}")
         destination = f"{shared}/_nav.html"
         template_path = f"{self.templates_path}/web/views/nav.html.template"
         self.render_template(template_path, destination)
+
+        lines = list(open(destination, 'r'))
+        last_line = self.resources_last_line(lines)
+
+        url = "{% url 'main:" + self.pluralize(self.model_name) + "' %}"
+        new_line = f"          <a class='dropdown-item' href='{url}'>{self.titleize(self.pluralize(self.model_name))}</a>\n"
+        if new_line not in lines:
+            lines.insert(last_line, new_line)
+            last_line += 1
+
+        with open(destination, 'w') as file:
+            file.writelines(lines)        
+
+
+    def resources_last_line(self, lines):
+        finish = 0
+
+        for index, line in enumerate(lines):
+            if 'EndResources' in line:
+                finish = index
+
+        return finish
 
     def writeable_attributes(self):
         atts = []
@@ -121,8 +152,12 @@ class WebResourceGenerator(GeneratorBase):
         current_file = f"{self.project_folder()}/main/views/__init__.py"
 
         lines = list(open(current_file, 'r'))
-        line = f"from .{self.pluralize(self.model_name)} import *\n"
 
+        line = f"from .main import *\n"
+        if line not in lines:
+            lines.append(line)
+
+        line = f"from .{self.pluralize(self.model_name)} import *\n"
         if line not in lines:
             lines.append(line)
         
@@ -134,6 +169,13 @@ class WebResourceGenerator(GeneratorBase):
         for view in views:
             self.render_view_template(view)
     
+    def render_main_view_template(self):
+        template_path = f"{self.templates_path}/web/main_view.template"
+        destination_folder = f"{self.project_folder()}/main/views/"
+        os.system(f"mkdir -p {destination_folder}")
+        destination = f"{destination_folder}/main.py"
+        self.render_template(template_path, destination)
+
     def render_view_template(self, view):
         template_path = f"{self.templates_path}/web/views/{view}.html.template"
         destination_folder = f"{self.project_folder()}/main/templates/{self.pluralize(self.model_name)}"
@@ -150,6 +192,7 @@ class WebResourceGenerator(GeneratorBase):
                 lines[index] = line.replace('path', 'path, include')
 
         new_lines = [
+            f"path('', views.main.index, name='main.index')",
             f"path('{self.pluralize(self.model_name)}/', views.{self.pluralize(self.model_name)}.index, name='{self.pluralize(self.model_name)}')",
             f"path('{self.pluralize(self.model_name)}/new', views.{self.pluralize(self.model_name)}.new, name='new_{self.model_name}')",
             f"path('{self.pluralize(self.model_name)}/<int:{self.model_name}_id>', views.{self.pluralize(self.model_name)}.show, name='{self.model_name}')",
