873af702804b5bbb50427fdb85d263d87e76edbd
Marcelo Ribeiro <me@marceloribeiro.us>
2020-11-27 05:13:02 -0800

    User auth from android

--- takeoff/generators/android/android_authentication_generator.py
+++ takeoff/generators/android/android_authentication_generator.py
@@ -268,7 +268,6 @@ class AndroidAuthenticationGenerator(AndroidBaseGenerator):
         self.add_attribute_to_entity('User', 'last_name', 'String')
         self.add_attribute_to_entity('User', 'email', 'String')
         self.add_attribute_to_entity('User', 'password', 'String', True)
-        self.add_attribute_to_entity('User', 'password_confirmation', 'String', True)
         self.add_attribute_to_entity('User', 'token', 'String', True, True)
     
     def add_login_strings(self):
