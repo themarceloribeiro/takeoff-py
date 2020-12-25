3d85a51d836f3b0689220776480aec1e27ddb0af
Marcelo Ribeiro <me@marceloribeiro.us>
2020-12-25 11:53:35 -0800

    Form with dynamic attrs

--- takeoff/generators/android/android_resource_generator.py
+++ takeoff/generators/android/android_resource_generator.py
@@ -9,14 +9,64 @@ class AndroidResourceGenerator(AndroidBaseGenerator):
     def __init__(self, name, options):
         super().__init__(name, options)
         self.entity_name = self.options.pop(0)
+        self.entity_attributes = []
 
     def run(self):
         self.setup()
         print(f">>> Running Android Resource Generator: {self.entity_name}")
+        self.load_resource_attributes()
         self.render_resource_form_fragment()
         self.render_resource_list_fragment()
         self.render_resource_list_adapter()
     
+    def load_resource_attributes(self):
+        entity_file = f"{self.project_folder()}/app/src/main/java/{self.android_prefix.replace('.', '/')}/models/{self.entity_name}.kt"
+        lines = list(open(entity_file, 'r'))
+
+        android_entity_attribute_types = [
+            'Int?',
+            'String?',
+            'Date?',
+        ]
+
+        for line in lines:
+            for field_type in android_entity_attribute_types:
+                if f": {field_type} = " in line:
+                    attribute_name = line.strip().split(":")[0].strip().replace('var ', '')
+                    attribute_type = field_type.replace('?', '')
+                    if attribute_name not in ['created_at', 'updated_at']:
+                        attribute_definition = { 
+                            'attribute_name': attribute_name, 
+                            'attribute_type': attribute_type,
+                            'field_type': self.field_type_for_attribute(attribute_type),
+                            'to_string_modifier': self.to_string_modifier(attribute_type),
+                            'from_string_modifier': self.from_string_modifier(attribute_type)
+                        }
+
+                        self.entity_attributes.append(attribute_definition)
+
+    def to_string_modifier(self, attribute_type):
+        if attribute_type == 'String':
+            return ''
+        elif attribute_type == 'Date':
+            return f"?.to{attribute_type}String()"            
+        else:
+            return '.toString()'
+    
+    def from_string_modifier(self, attribute_type):
+        if attribute_type == 'String':
+            return ''
+        else:
+            return f".to{attribute_type}()"
+
+    def field_type_for_attribute(self, attribute_type):
+        types = {
+            'Int': 'EditText',
+            'String': 'EditText',
+            'Date': 'EditText'
+        }
+        return types.get(attribute_type)
+
     def render_resource_form_fragment(self):
         package_path = self.android_prefix.replace('.', '/')
         template_path = f"{self.templates_path}/app/src/main/java/resource/ResourceFormFragment.kt.template"
